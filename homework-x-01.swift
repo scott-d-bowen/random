de2a5e38ea30b54e40189bc0fcca6e95e52e0b2f
Scott D. Bowen <49947173+scott-d-bowen@users.noreply.github.com>
2021-12-04 17:25:13 +1100

    Update homework-x-01.swift

--- homework-x-01.swift
+++ homework-x-01.swift
@@ -1,6 +1,6 @@
 //  main.swift
 //  R4: Make Difficult
-//  'Mark V'
+//  'Mark IX.B'
 //  Created by Scott Bowen
 
 import Foundation
@@ -10,71 +10,85 @@ import Compression
 let DIFFICULTY = 144    // Warning Use: ~161 to 186 only!
 
 print("Hello, World!")
+
+print("Init Random Data.......")
+let bigNum  = BigUInt.randomInteger(withExactWidth: 8*32768*256*8)
+let bigData = bigNum.serialize().toArray(type: UInt8.self).chunked(into: 256)
+print("bigData.count:", bigData.count)
+print("RNG CMPL [OK]")
+
 var date_start = Date()
+print(date_start)
 
+DispatchQueue.concurrentPerform(iterations: 8, execute: { indexGCD in
+    var bulkStats: [UInt8] = [ ]
 
-var bulkStats: [UInt8] = [ ]
+    var difficultCount = 0
+    var VIPs: [UInt8] = [ ]
 
-var difficultCount = 0
-var VIPs: [UInt8] = [ ]
+    for _8MB_LOOP_ in 0..<32768 {
+        // let bigNum = BigUInt.randomInteger(withExactWidth: 256*8)
+        // bigData.subdata(in: (8*1024*1024*indexGCD)..<(8*1024*1024*indexGCD+(8*1024*1024)))
+        var data: [UInt8] = [ ]
+        data.append(contentsOf: bigData[indexGCD * 32768 + _8MB_LOOP_])
+        // print("data.count:", data.count)
+        
+        var stats: [UInt8] = Array(repeating: 0x00, count: 256)
+        // var dataUInt8: [UInt8] = [ ]
+        var first160UniqueValues: [UInt8] = [ ]
+        var VIPs_Small: [UInt8] = [ ]
 
-for _ in 0..<32768 {
-    let bigNum = BigUInt.randomInteger(withExactWidth: 256*8)
-    let data   = bigNum.serialize()
-    var stats: [UInt8] = Array(repeating: 0x00, count: 256)
-    var dataUInt8: [UInt8] = [ ]
-    var first160UniqueValues: [UInt8] = [ ]
-    var VIPs_Small: [UInt8] = [ ]
-
-    for value in (0)..<(data.count) {
-        stats[Int(data[value])] += 1
-    }
-    
-    var uniqueValues = 0
-    for value in (0)..<(stats.count) {
-        if (stats[value] >= 1) {
-            uniqueValues += 1
-            if (uniqueValues <= 160) {
-                first160UniqueValues.append(UInt8(value))
-            }
+        for value in (0)..<(data.count) {
+            stats[Int(data[value])] += 1
         }
-    }
-    first160UniqueValues.sort()
-    
-    if (uniqueValues >= DIFFICULTY) {
-        // TODO: print("C:", first160UniqueValues.count)
-        difficultCount += 1
-        dataUInt8 = data.toArray(type: UInt8.self)
-        dataUInt8.sort()
-        // TODO: print("*:", uniqueValues)
-        for i in 0..<16 {
-            for j in 0..<16 {
-                if (dataUInt8[i*16+j] > first160UniqueValues.last!) {
-                    // TODO: print(dataUInt8[i*16+j], terminator: ", ")
-                    VIPs_Small.append(dataUInt8[i*16+j])
-                    // if (j==15) { print() }
+        
+        var uniqueValues = 0
+        for value in (0)..<(stats.count) {
+            if (stats[value] >= 1) {
+                uniqueValues += 1
+                if (uniqueValues <= 160) {
+                    first160UniqueValues.append(UInt8(value))
                 }
             }
         }
-        if (VIPs_Small.count >= 1) {
-            VIPs_Small.insert(UInt8(VIPs_Small.count-1), at: 0)
-            VIPs.append(contentsOf: VIPs_Small)
-        } else {
-            VIPs.append(0x00)
+        first160UniqueValues.sort()
+        
+        if (uniqueValues >= DIFFICULTY) {
+            // TODO: print("C:", first160UniqueValues.count)
+            difficultCount += 1
+            // dataUInt8 = data // TODO: .toArray(type: UInt8.self)
+            data.sort()
+            // TODO: print("*:", uniqueValues)
+            for i in 0..<16 {
+                for j in 0..<16 {
+                    if (data[i*16+j] > first160UniqueValues.last!) {
+                        // TODO: print(dataUInt8[i*16+j], terminator: ", ")
+                        VIPs_Small.append(data[i*16+j])
+                        // if (j==15) { print() }
+                    }
+                }
+            }
+            if (VIPs_Small.count >= 1) {
+                VIPs_Small.insert(UInt8(VIPs_Small.count-1), at: 0)
+                VIPs.append(contentsOf: VIPs_Small)
+            } else {
+                VIPs.append(0x00)
+            }
         }
+        stats.sort()
+        bulkStats.append(contentsOf: stats)
     }
-    stats.sort()
-    bulkStats.append(contentsOf: stats)
-}
-print()
-print()
-print("difficultCount:", difficultCount)
+    print()
+    print()
+    print("difficultCount:", difficultCount)
 
-let compVIPs = try NSData(data: Data(fromArray: VIPs)).compressed(using: .lzma)
-print(compVIPs.length, VIPs.count, VIPs.first!, VIPs.last!)
+    let compVIPs = try! NSData(data: Data(fromArray: VIPs)).compressed(using: .lzfse)
+    print(compVIPs.length, VIPs.count, VIPs.first, VIPs.last)
 
-let comp = try NSData(data: Data(fromArray: bulkStats)).compressed(using: .lzma)
-print(comp.length, bulkStats.count, bulkStats.first!, bulkStats.last!)
+    // TODO: Re-enable this BulkStats Compression
+    // let comp = try! NSData(data: Data(fromArray: bulkStats)).compressed(using: .lz4)
+    // print(comp.length, bulkStats.count, bulkStats.first!, bulkStats.last!)
+})
 
 print()
 
